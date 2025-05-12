40c96c81e279f4dafbd41e6fc1ab8121e9714058
LoparPanda <60553740+LoparPanda@users.noreply.github.com>
2025-05-11 18:13:32 -0700

    delinting pass

--- src/binary_reader.rs
+++ src/binary_reader.rs
@@ -1,4 +1,4 @@
-use std::io::{Result, Error, Read, Seek, SeekFrom, ErrorKind};
+use std::io::{Read, Result, Seek, SeekFrom};
 
 pub trait ReadAll : Read {
     fn read_all(&mut self, buf: &mut [u8]) -> Result<()>;
@@ -7,6 +7,7 @@ pub trait ReadAll : Read {
 pub trait BinaryRead : ReadAll {
     fn read_u8(&mut self) -> Result<u8>;
     fn read_le_u16(&mut self) -> Result<u16>;
+    #[allow(dead_code)]
     fn read_le_i32(&mut self) -> Result<i32>;
 }
 
@@ -34,30 +35,26 @@ impl<R: Seek> Seek for BinaryReader<R> {
 
 impl<R: Read> ReadAll for BinaryReader<R> {
     fn read_all(&mut self, buf: &mut [u8]) -> Result<()> {
-        match self.inner.read(buf) {
-            Ok(len) if len == buf.len() => Ok(()),
-            Ok(_) => Err(Error::new(ErrorKind::Other, "Could not read all bytes")),
-            Err(e) => Err(e)
-        }
+      self.inner.read_exact(buf)
     }
 }
 
 impl<R: Read> BinaryRead for BinaryReader<R> {
     fn read_u8(&mut self) -> Result<u8> {
         let mut buf = [0; 1];
-        try!(self.read_all(&mut buf));
+        self.read_all(&mut buf)?;
         Ok(buf[0])
     }
 
     fn read_le_u16(&mut self) -> Result<u16> {
         let mut buf = [0; 2];
-        try!(self.read_all(&mut buf));
+        self.read_all(&mut buf)?;
         Ok(((buf[1] as u16) << 8) | (buf[0] as u16))
     }
 
     fn read_le_i32(&mut self) -> Result<i32> {
         let mut buf = [0; 4];
-        try!(self.read_all(&mut buf));
+        self.read_all(&mut buf)?;
         Ok((
             ((buf[3] as u32) << 24) | ((buf[2] as u32) << 16) |
             ((buf[1] as u32) << 8) | (buf[0] as u32)) as i32)
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
+#![feature(read_buf, core_io_borrowed_buf)]
+
 mod binary_reader;
 pub mod spc;
